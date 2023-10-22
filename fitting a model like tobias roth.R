d94e88a8c948b4cb8fa741d0f2b793c791c8c8f4
isaac-finkelstein <Isaacfinkelstein2@gmail.com>
2023-10-22 17:11:08 -0400

    These two files are my updates from today. Note that it is a simple model that is not ecologically interesting (obviously, more eggs hatched will correlate with success).
    The data is also uploaded.

--- fitting a model like tobias roth.R
+++ fitting a model like tobias roth.R
@@ -1,5 +1,5 @@
 #This script is to get data for my stan model
-setwd("/Users/isaacfinkelstein/Documents/Carleton/courses/bayesian/research project/nest_survival_repo")
+setwd("/Users/isaacfinkelstein/Documents/Carleton/courses/bayesian/research project/nest_survival_repo/raw_data")
 library(raster)
 library(dplyr)
 library(tidyverse)
@@ -10,7 +10,7 @@ all_data<-read.csv("Final_Nest_Monitoring_EABA&COAT_ALL_years.csv")
 
 east_bay_only_data <- subset(all_data, site== "East Bay Mainland")
 
-#nest, my response variable is nest fate (success or failed):
+#my response variable is nest fate (success or failed):
 #so I need to clean that variable -- remove unknowns
 
 clean_nest_fate_data <- filtered_fate_data<-east_bay_only_data %>%
@@ -23,38 +23,108 @@ str(Nnest)
 #I want to create a model that is like tobiasroth.github.io/BDAEcology
 #so I need predictor variables -- first observation, last observation, maxage, etc.
 
+
 #by floating the eggs, we estimate the day the nest was initiated
 #this is called Estimated.Nest.Initiation.Date
 str(clean_nest_fate_data$Estimated.Nest.Initiation.Date)
 #this is not an integer
+#Turn it into an integer
+#the following code counts the number of seconds since 1/1/1970
+clean_nest_fate_data$date_init=as.POSIXct(clean_nest_fate_data$Estimated.Nest.Initiation.Date, format = "%d-%b-%Y")
+clean_nest_fate_data$first_day_as_int_sec<- as.integer(clean_nest_fate_data$date_init)
+View(clean_nest_fate_data$first_day_as_int)
+#to view this as number of days I need to divide by 86400
+#I use the floor function to round this to a whole number (without this, each value is .17 e.g. 10412.17)
+clean_nest_fate_data$first_day_as_int_days <- floor(as.integer(clean_nest_fate_data$first_day_as_int_sec) / 86400)
+View(clean_nest_fate_data$first_day_as_int_days)
+#this is days since 1/1/1970
+#there's NA's  - so I use in<lower=0> in my stan model. So any negative value will not affect the model, I think
+clean_nest_fate_data$first_day_as_int_days[is.na(clean_nest_fate_data$first_day_as_int_days) ] <- -1
+
 
 #the last day of the nest depends on if it's alive or dead (success or failed)
 #if alive -> then it's the hatch day = Hatch_Date
 #if dead, then it's the Last.Active day. 
 
 #so, let's make that one variable
-east_bay_only_data$last_observed <- ifelse(east_bay_only_data$Fate == "success",
-                                           east_bay_only_data$Hatch_Date, east_bay_only_data$Last.Active)
-View(east_bay_only_data)
-east_bay_only_data$last_observed
+clean_nest_fate_data$last_observed <- ifelse(clean_nest_fate_data$Fate == "success",
+                                           clean_nest_fate_data$Hatch_Date, clean_nest_fate_data$Last.Active)
+
+clean_nest_fate_data$last_observed
+str(clean_nest_fate_data$last_observed)
+#make it an integer
+#the following code counts the number of seconds since 1/1/1970
+clean_nest_fate_data$last_day=as.POSIXct(clean_nest_fate_data$last_observed, format = "%d-%b-%Y")
+clean_nest_fate_data$last_day_as_int<- as.integer(clean_nest_fate_data$last_day)
+View(clean_nest_fate_data$last_day_as_int)
+str(clean_nest_fate_data$last_day_as_int)
+#to view this as number of days I need to divide by 86400
+clean_nest_fate_data$last_day_as_int_days <- floor(as.integer(clean_nest_fate_data$last_day_as_int) / 86400)
+View(clean_nest_fate_data$last_day_as_int_days)
+#this is days since 1/1/1970
+
+#there's NA's  - so I use in<lower=0> in my stan model. So any negative value will not affect the model, I think
+clean_nest_fate_data$last_day_as_int_days[is.na(clean_nest_fate_data$last_day_as_int_days) ] <- -1
+
+
 
 #NOTE THAT I HAVE AN IRREGULAR NEST CONTROL BECAUSE WE DON'T CHECK EVERY DAY
 #for now, I'm ignoring that, but later I need to do the irregular model
 #wait, it seems that there's a Mayfield end date formula -- so they already did this stuff for me - Yay!
 
 #The Mayfield days formula (Mayfield_days..formula.) gives the number of days the nest was active, I believe
+mayfield_days_formula<-clean_nest_fate_data$Mayfield_days..formula.
+View(mayfield_days_formula)
+str(mayfield_days_formula)
 
-#Maxage in the tobias roth example is described as "maximum of last"
+#is it the same as last-first?
+clean_nest_fate_data$total_days_nest_active<-clean_nest_fate_data$last_day_as_int_days - clean_nest_fate_data$first_day_as_int_days
+View(clean_nest_fate_data$total_days_nest_active)
+#it is not the same. Probably because the mayfield formula isn't an exact count -- I'm guessing it takes into acount the interval betweeen checks
+#so the Mayfield formula may be suitable for an irregular nest control model -- but for now let's assume a regular nest control.
 
-maxage <- east_bay_only_data$Mayfield_days..formula.
-#this made a chr variable
-#so lets set this to an interger
-str(maxage)
-int_maxage <- as.integer(maxage)
+#this was giving me "numeric(0)" so something went wrong.
+#stan does not like NAs so I drop them
+#leaving this here in case I want it later
+#clean_nest_fate_data <- clean_nest_fate_data[complete.cases(clean_nest_fate_data), ]
+#View(clean_nest_fate_data$total_days_nest_active)
 
-#so age should be length int_maxage...
 
-str(east_bay_only_data)
+#must make it a numeric variable, because there are values like 1.5 -> so not an integer
+clean_nest_fate_data$Mayfield_days..formula.<-as.numeric(clean_nest_fate_data$Mayfield_days..formula.)
+#there are NA's which I think STAN does not like.
+#so I'm just going to ignore missing values
+clean_nest_fate_data[!is.na(clean_nest_fate_data$X._of_Eggs_Hatched), ]
+str(clean_nest_fate_data$Mayfield_days..formula.)
 
-#okay, I'm not going to get anywhere until I understand what is "age" in tobias roth -- why is age -1.65, -1.54, etc. 
+#okay, I'm not going to get anywhere until I understand what is "age" in tobias roth -- why is age -1.65, -1.54, etc
+#answer, I believe it is standardized, that's why it can be negative etc.>
+
+#their maxage is an integer with the maximum number of days alive
+#to do this with the Mayfield formula:
+#max(clean_nest_fate_data$Mayfield_days..formula., na.rm=TRUE)
+#maxage <- as.integer(38)
+#str(maxage)
+
+maxage<-max(clean_nest_fate_data$last_day_as_int_days, na.rm=TRUE)
+View(maxage)
+
+#Their y variable seems to be a measure of if the nest is alive or not - probably per day
+#hmmm, don't really understand that
+str(clean_nest_fate_data$Fate)
+
+#I'm just going to keep it simple and add one predictor variable -- number of eggs hatched
+#this should be correlated with the length of time the nest was active
+#nests that were successful will be active longer, will hatch more eggs
+str(clean_nest_fate_data$X._of_Eggs_Hatched)
+
+clean_nest_fate_data$X._of_Eggs_Hatched<-as.integer(clean_nest_fate_data$X._of_Eggs_Hatched)
+str(clean_nest_fate_data$X._of_Eggs_Hatched)
+#there are NA's which I think STAN does not like.
+#so I'm just going to ignore missing values
+clean_nest_fate_data[!is.na(clean_nest_fate_data$X._of_Eggs_Hatched), ]
+str(clean_nest_fate_data$X._of_Eggs_Hatched)
+
+#I'm renaming the variable here
+x_of_eggs <- clean_nest_fate_data$X._of_Eggs_Hatched
 
--- make stan model like tobias roth.stan
+++ make stan model like tobias roth.stan
@@ -1,9 +1,10 @@
 data {
   int<lower=0> Nnests; //number of nests
-  real Mayfield_days..formula.[Nnests]; //how many days the nest was active
-  int<lower=0> maxage; //maximum of mayfield days formula
+  int<lower=0> first_day_as_int_days[Nnests]; //for each of nNests, day (integer) it was first observed = days since 1/1/1070
+  int<lower=0> last_day_as_int_days[Nnests]; //for each of Nnests, the day it was last observed = days since 1/1/1970
+  int<lower=0> maxage; //maximum of last (is 17740 because it's days since 1/1/1970)
   int<lower=0> y[Nnests, maxage]; //indicator of alive nests
-  int<lower=0> X_of_eggs[Nnests]; //a covariate of the nests
+  int<lower=0> x_of_eggs[Nnests]; //a covariate of the nests
 }
 
 parameters {
@@ -11,11 +12,11 @@ parameters {
 }
 
 model {
-  real S[Nnests, Mayfield_days..formula., maxage-1]; //survival probability
+  real S[Nnests, maxage-1]; //survival probability
 
   for(i in 1:Nnests){
-    for(t in first [i]: (last[i]-1)){
-      S[i,t] = inv_logit(b[1] + b[2]*X_of_eggs);
+    for(t in first_day_as_int_days[i]: (last_day_as_int_days[i]-1)){
+      S[i,t] = inv_logit(b[1] + b[2]*x_of_eggs[i]); // I haven't done b[3]*age[t] here!
     }
   }
   
@@ -23,7 +24,7 @@ model {
   
   //likelihood
   for(i in 1:Nnests) {
-    for (t in (first [i] +1): last [i]){
+    for (t in (first_day_as_int_days [i] +1): last_day_as_int_days [i]){
       y[i,t] ~bernoulli(y[i,t-1]*S[i,t-1]);
     }
   }
