a5a0ca96e1d09615ba610f06a186d3494b0eb3a9
isaac-finkelstein <Isaacfinkelstein2@gmail.com>
2023-11-06 10:50:39 -0500

    Small updates

--- run_stan_like_tobias.R
+++ run_stan_like_tobias.R
@@ -20,16 +20,18 @@ stan_data <- readRDS("stan_data_list.rds")
 ## try cmdstanr: https://mc-stan.org/cmdstanr/articles/cmdstanr.html
 library(cmdstanr) # I prefer this interface to Stan - it's more up to date, and it gives nicer error messages
 
-mod_prep <- cmdstanr::cmdstan_model(stan_file = "stan_model_like_tobias_roth.stan")
+mod_prep <- cmdstanr::cmdstan_model(stan_file = "make_stan_model_like_tobias_roth.stan")
 
 run_stan_model_like_tobias<- mod_prep$sample(data=stan_data,
-                                             refresh = 100,
-                                             iter_sampling = 100, #just for initial attempts, but use the defaults when running for real
-                                             iter_warmup = 500,
-                                             parallel_chains = 4)#just for initial attempts, but use the defaults when running for real
+                                             refresh = 100, #default is 200, or 500
+                                             iter_sampling = 100, #just for initial attempts, but use the defaults when running for real - default =1000
+                                             iter_warmup = 500, #default = 1000
+                                             parallel_chains = 4)#just for initial attempts, but use the defaults when running for real - default = 4
 
 summ_model <- run_stan_model_like_tobias$summary()
+View(summ_model)
 
 
-
+library(boot)
+inv.logit(0.107)
 
